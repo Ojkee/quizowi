3827b687c9c4dc491b0eb4caed4fbb525b9b806b
Ojkee <17oju17@gmail.com>
2025-10-04 18:02:54 +0200

    2025-10-04; Client; Echo server for now

--- src/client/core.py
+++ src/client/core.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 
 
@@ -8,14 +9,45 @@ class ClientSocket:
     async def _connect(self) -> None:
         reader, writer = await asyncio.open_connection("localhost", self._port)
 
-        writer.write(b"hello")
-        await writer.drain()
+        listener = asyncio.create_task(self._listener(reader))
+        sender = asyncio.create_task(self._sender(writer))
 
-        data = await reader.read(100)
-        print(data.decode("utf-8"))
+        _, pending = await asyncio.wait(
+            {listener, sender}, return_when=asyncio.FIRST_COMPLETED
+        )
+
+        for task in pending:
+            task.cancel()
 
         writer.close()
         await writer.wait_closed()
+        print("disconnected")
+
+    async def _listener(self, reader: asyncio.StreamReader) -> None:
+        try:
+            while True:
+                server_msg = await reader.read(1024)
+                if not server_msg:
+                    break
+                print(server_msg)
+        except asyncio.CancelledError:
+            pass
+
+    async def _sender(self, writer: asyncio.StreamWriter) -> None:
+        try:
+            while True:
+                msg = await self._wait_for_input("> ")
+                if msg.lower() == "quit":
+                    break
+                writer.write(msg.encode())
+                await writer.drain()
+        except asyncio.CancelledError:
+            pass
+
+    async def _wait_for_input(self, ss: str) -> str:
+        sys.stdout.write(ss)
+        sys.stdout.flush()
+        return await asyncio.get_event_loop().run_in_executor(None, sys.stdin.readline)
 
     def connect(self) -> None:
         asyncio.run(self._connect())
